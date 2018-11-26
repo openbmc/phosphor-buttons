fb35a325954f6b99547e19bcb0e2d68112985513
Matt Spinler <spinler@us.ibm.com>
2018-11-26 14:30:30 -0600

    Add button-handler framework
    This new application will listen for button press
    signals generated by the xyz.openbmc_project.Chassis.Buttons
    code and perform the necessary actions, like powering off
    when the power button is pressed.
    
    This first commit is just the framework, and all of the
    functionality will follow.
    
    Change-Id: Ia877684627b53e0fcc6f4b0b82063811e8ced6ea
    Signed-off-by: Matt Spinler <spinler@us.ibm.com>

---
+++ src/button_handler.cpp
@@ -0,0 +1,11 @@
+#include "button_handler.hpp"
+
+namespace phosphor
+{
+namespace button
+{
+Handler::Handler(sdbusplus::bus::bus& bus) : bus(bus)
+{
+}
+} // namespace button
+} // namespace phosphor
