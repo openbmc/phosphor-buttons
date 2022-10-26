22198a6155de7d6e880da939643169fcf103b471
Naveen Moses <naveen.mosess@hcl.com>
2022-10-26 21:27:52 +0530

    I2C based host position read support
    This change extends host selector class to read host position from
    direct gpio or i2c based host position value.
    The host selector config has two types namely "gpio" and
    "i2c" based on this this functionality is decided.
    
    Signed-off-by: Naveen Moses <naveen.mosess@hcl.com>
    Change-Id: Icd7ef7f40a3580210eef50f0b43d060d57e21786

--- inc/hostSelector_switch.hpp
+++ inc/hostSelector_switch.hpp
@@ -8,30 +8,34 @@
 #include "xyz/openbmc_project/Chassis/Buttons/HostSelector/server.hpp"
 #include "xyz/openbmc_project/Chassis/Common/error.hpp"
 
+#include <fcntl.h>
+#include <sys/inotify.h>
+#include <sys/ioctl.h>
 #include <unistd.h>
 
 #include <nlohmann/json.hpp>
 #include <phosphor-logging/elog-errors.hpp>
+#include <phosphor-logging/lg2.hpp>
 
 #include <fstream>
 #include <iostream>
+extern "C"
+{
+#include <i2c/smbus.h>
+#include <linux/i2c-dev.h>
+}
 
 static constexpr std::string_view HOST_SELECTOR = "HOST_SELECTOR";
-
+using HostSelectorIntf = sdbusplus::server::object_t<
+    sdbusplus::xyz::openbmc_project::Chassis::Buttons::server::HostSelector>;
 static constexpr auto INVALID_INDEX = std::numeric_limits<size_t>::max();
 
-class HostSelector final :
-    public sdbusplus::server::object_t<
-        sdbusplus::xyz::openbmc_project::Chassis::Buttons::server::
-            HostSelector>,
-    public ButtonIface
+class HostSelector final : public HostSelectorIntf, public ButtonIface
 {
   public:
     HostSelector(sdbusplus::bus_t& bus, const char* path, EventPtr& event,
                  buttonConfig& buttonCfg) :
-        sdbusplus::server::object_t<sdbusplus::xyz::openbmc_project::Chassis::
-                                        Buttons::server::HostSelector>(
-            bus, path, action::defer_emit),
+        HostSelectorIntf(bus, path, action::defer_emit),
         ButtonIface(bus, event, buttonCfg)
     {
         init();
@@ -39,8 +43,21 @@ class HostSelector final :
         hsPosMap = buttonCfg.extraJsonInfo.at("host_selector_map")
                        .get<std::map<std::string, int>>();
         maxPosition(buttonCfg.extraJsonInfo["max_position"], true);
-        gpioLineCount = buttonCfg.gpios.size();
-        setInitialHostSelectorValue();
+        type = buttonCfg.extraJsonInfo.at("type");
+
+        if (type == "i2c")
+        {
+            size_t busId = buttonCfg.extraJsonInfo.at("busId");
+            offset = buttonCfg.extraJsonInfo.at("offset");
+            deviceAddress = buttonCfg.extraJsonInfo.at("address");
+
+            i2cBus = "/dev/i2c-" + std::to_string(busId);
+        }
+        else
+        {
+            gpioLineCount = buttonCfg.gpios.size();
+            setInitialHostSelectorValue();
+        }
         emit_object_added();
     }
 
@@ -63,11 +80,55 @@ class HostSelector final :
     size_t getGpioIndex(int fd);
     void setInitialHostSelectorValue(void);
     void setHostSelectorValue(int fd, GpioState state);
+    using HostSelectorIntf::position;
+    size_t position() const override
+    {
+        if (type == "i2c")
+        {
+            int i2cFd = open(i2cBus.c_str(), O_RDWR);
+            int error = 0;
+            int value = 0;
+            if (i2cFd < 0)
+            {
+                lg2::error("{TYPE}: i2c fd open error: {ERROR}", "TYPE",
+                           getFormFactorType(), "ERROR", i2cFd);
+                error++;
+            }
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg)
+            if (ioctl(i2cFd, I2C_SLAVE_FORCE, deviceAddress) < 0)
+            {
+                lg2::error("{TYPE}:cannot set i2c address: {ERROR}", "TYPE",
+                           getFormFactorType(), "ERROR", i2cFd);
+                close(i2cFd);
+                error++;
+            }
+            value = i2c_smbus_read_byte_data(i2cFd, offset);
+            if (value < 0)
+            {
+                lg2::error("{TYPE}: i2c read failed: {ERROR}", "TYPE",
+                           getFormFactorType(), "ERROR", value);
+                close(i2cFd);
+                error++;
+            }
+            close(i2cFd);
+            if (error)
+            {
+                lg2::error("{TYPE}:i2c read rror: {ERROR}", "TYPE",
+                           getFormFactorType(), "ERROR", errno);
+                value = 0;
+            }
+            return value;
+        }
+        return HostSelectorIntf::position();
+    }
 
   protected:
     size_t hostSelectorPosition = 0;
     size_t gpioLineCount;
-
+    uint8_t deviceAddress;
+    uint8_t offset;
+    std::string i2cBus;
+    std::string type;
     // map of read Host selector switch value and corresponding host number
     // value.
     std::map<std::string, int> hsPosMap;
