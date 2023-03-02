bde02b623141d4524c6b8b3d4bdef3366c5fed10
DelphineCCChiu <Delphine_CC_Chiu@wiwynn.com>
2023-03-02 15:29:23 +0800

    Support multi power buttons with pressing time
    Support multiple power buttons behaviors
    
    Tested:
    Press buttons and check corresponding behaviors.
    - Chassis power button:
     - > 4 seconds: chassis power cycle
    - Slot power button:
     - <= 4 seconds: chassis power on
     - > 4 seconds but <= 8 seconds: chassis power cycle
     - > 8 seconds: chassis power off
    
    Change-Id: I7789f0367d5e846dd9e68f966ba0755fc916217a
    Signed-off-by: DelphineCCChiu <Delphine_CC_Chiu@wiwynn.com>

--- inc/button_handler.hpp
+++ inc/button_handler.hpp
@@ -1,6 +1,10 @@
 #pragma once
+#include "config.h"
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/bus/match.hpp>
+#include <string>
+#include <sstream>
+#include <string>
 
 namespace phosphor
 {
@@ -10,11 +14,27 @@ enum class PowerEvent
 {
     powerPressed,
     longPowerPressed,
+    longerPowerPressed,
     resetPressed,
     powerReleased,
     longPowerReleased,
     resetReleased
 };
+
+inline static int numberOfChassis()
+{
+    int chassisnum;
+    size_t found = 0;
+
+    for (int i = 0; found != std::string::npos; i++)
+    {
+        found = static_cast<std::string>(INSTANCES).find(std::to_string(i));
+        chassisnum = i - 1;
+    }
+
+    return chassisnum;
+}
+
 /**
  * @class Handler
  *
@@ -53,16 +73,6 @@ class Handler
      * @param[in] msg - sdbusplus message from signal
      */
     void powerReleased(sdbusplus::message_t& msg);
-
-    /**
-     * @brief The handler for a long power button press
-     *
-     * If the system is currently powered on, it will
-     * perform an immediate power off.
-     *
-     * @param[in] msg - sdbusplus message from signal
-     */
-    void longPowerPressed(sdbusplus::message_t& msg);
 
     /**
      * @brief The handler for an ID button press
@@ -138,7 +148,7 @@ class Handler
      *
      * @return void
      */
-    void handlePowerEvent(PowerEvent powerEventType);
+    void handlePowerEvent(PowerEvent powerEventType, std::string chassisNum);
 
     /**
      * @brief sdbusplus connection object
@@ -154,6 +164,12 @@ class Handler
      * @brief Matches on the power button long press released signal
      */
     std::unique_ptr<sdbusplus::bus::match_t> powerButtonLongPressed;
+
+    /**
+     * @brief Matches on the multi power button released signal
+     */
+    std::vector<std::shared_ptr<sdbusplus::bus::match_t>>
+        multiPowerButtonReleased;
 
     /**
      * @brief Matches on the ID button released signal
