d052a244c275e75b7c0f5cb6e5714f5979277ee6
DelphineCCChiu <Delphine_CC_Chiu@wiwynn.com>
2023-05-12 15:46:41 +0800

    Support multi power buttons with multi behaviors
    For supporting more-than-one power buttons behaviors,
    add new matches and instances by following json config.
    
    This change is for multiple slots integrated on one chassis,
    and each slot has button to control power status.
    
    For example:
    /xyz/openbmc_project/Chassis/Buttons/Power00 for button on sled
    /xyz/openbmc_project/Chassis/Buttons/Power01 for button on slot1
    /xyz/openbmc_project/Chassis/Buttons/Power02 for button on slot2
    
    Moreover, support multi-level power control by json config,
    chassis now can do action by correspnding pressing duration.
    
    Tested:
    Press buttons and check corresponding behaviors.
    
    Change-Id: I7789f0367d5e846dd9e68f966ba0755fc916217a
    Signed-off-by: DelphineCCChiu <Delphine_CC_Chiu@wiwynn.com>

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -14,9 +14,9 @@
 // limitations under the License.
 */
 
-#include "config.h"
-
 #include "gpio.hpp"
+
+#include "config.hpp"
 
 #include <error.h>
 #include <fcntl.h>
@@ -96,6 +96,7 @@ uint32_t getGpioBase()
     // Look for a /sys/class/gpio/gpiochip*/label file
     // with a value of GPIO_BASE_LABEL_NAME.  Then read
     // the base value from the 'base' file in that directory.
+
 #ifdef LOOKUP_GPIO_BASE
     for (auto& f : fs::directory_iterator(gpioDev))
     {
