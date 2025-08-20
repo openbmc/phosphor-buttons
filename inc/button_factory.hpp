openbmc/phosphor-buttons

commit 31ce375e7e71ff6b3da460dc63c1ab4f16cf4df6

Support multi power buttons with multi behaviors
For supporting more-than-one power buttons behaviors,
add new matches and instances by following json config.

This change is for multiple slots integrated on one chassis,
and each slot has button to control power status.

For example:
/xyz/openbmc_project/Chassis/Buttons/Power01 for button on slot1
/xyz/openbmc_project/Chassis/Buttons/Power02 for button on slot2

Moreover, support multi-level power control by json config,
chassis now can do action by corresponding pressing duration.

Tested:
Press buttons and check corresponding behaviors.

Change-Id: I7789f0367d5e846dd9e68f966ba0755fc916217a
Signed-off-by: Rush Chen <rush.chen.wiwynn@gmail.com>

inc/button_factory.hpp
@@ -35,13 +35,27 @@ class ButtonFactory
     template <typename T>
     void addToRegistry()
     {
-        buttonIfaceRegistry[std::string(T::getFormFactorName())] =
+        buttonIfaceRegistry[T::getFormFactorName()] =
             [](sdbusplus::bus_t& bus, EventPtr& event,
                ButtonConfig& buttonCfg) {
-                return std::make_unique<T>(bus, T::getDbusObjectPath(), event,
-                                           buttonCfg);
+                return std::make_unique<T>(bus, T::getDbusObjectPath().c_str(),
+                                           event, buttonCfg);
             };
     }
+
+    template <typename T>
+    void addToRegistry(size_t index)
+    {
+        auto indexStr = std::to_string(index);
+        buttonIfaceRegistry[T::getFormFactorName() + indexStr] =
+            [=](sdbusplus::bus_t& bus, EventPtr& event,
+                ButtonConfig& buttonCfg) {
+                return std::make_unique<T>(
+                    bus, (T::getDbusObjectPath() + indexStr).c_str(), event,
+                    buttonCfg);
+            };
+    }
+
     /**
      * @brief this method returns the button interface object
      *    corresponding to the button formfactor name provided
@@ -75,5 +89,16 @@ class ButtonIFRegister
     {
         // register the class factory function
         ButtonFactory::instance().addToRegistry<T>();
+    }
+
+    explicit ButtonIFRegister(size_t count)
+    {
+        // register the class factory function
+        // The index, 'countIter', starts at 1 and increments,
+        // representing slot_1 through slot_N.
+        for (size_t countIter = 1; countIter <= count; countIter++)
+        {
+            ButtonFactory::instance().addToRegistry<T>(countIter);
+        }
     }
 };

inc/button_handler.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include "config.hpp"
 #include "power_button_profile.hpp"
 
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/bus/match.hpp>
+
+#include <algorithm>
+#include <numeric>
+#include <sstream>
+#include <string>
 
 namespace phosphor
 {
@@ -16,6 +22,19 @@ enum class PowerEvent
     powerReleased,
     resetReleased
 };
+
+enum class PwrCtl
+{
+    chassisOn,
+    chassisOff,
+    chassisCycle,
+};
+
+inline static size_t numberOfChassis()
+{
+    return instances.size();
+}
+
 /**
  * @class Handler
  *
@@ -129,6 +148,7 @@ class Handler
      * @return void
      */
     void handlePowerEvent(PowerEvent powerEventType,
+                          const std::string& objectPath,
                           std::chrono::microseconds duration);
 
     /**
@@ -145,6 +165,12 @@ class Handler
      * @brief Matches on the power button long press released signal
      */
     std::unique_ptr<sdbusplus::bus::match_t> powerButtonLongPressed;
+
+    /**
+     * @brief Matches on the multi power button released signal
+     */
+    std::vector<std::unique_ptr<sdbusplus::bus::match_t>>
+        multiPowerButtonReleased;
 
     /**
      * @brief Matches on the ID button released signal
@@ -165,6 +191,17 @@ class Handler
      * @brief The custom power handler profile object.
      */
     std::unique_ptr<PowerButtonProfile> powerButtonProfile;
+
+    /**
+     * @brief Flag to indicate multi power button mode(false) or host select
+     * button mode(true)
+     */
+    bool hostSelectButtonMode = false;
+
+    /**
+     * @brief Flag to indicate if the button supports multi action
+     */
+    bool isButtonMultiActionSupport = true;
 };
 
 } // namespace button

inc/debugHostSelector_button.hpp
@@ -13,8 +13,7 @@
 #include <phosphor-logging/elog-errors.hpp>
 #include <phosphor-logging/lg2.hpp>
 
-static constexpr std::string_view DEBUG_SELECTOR_BUTTON =
-    "DEBUG_SELECTOR_BUTTON";
+static constexpr auto DEBUG_SELECTOR_BUTTON = "DEBUG_SELECTOR_BUTTON";
 
 class DebugHostSelector final :
     public sdbusplus::server::object_t<
@@ -44,12 +43,12 @@ class DebugHostSelector final :
     void simLongPress() override;
     void handleEvent(sd_event_source* es, int fd, uint32_t revents) override;
 
-    static constexpr std::string_view getFormFactorName()
+    static constexpr std::string getFormFactorName()
     {
         return DEBUG_SELECTOR_BUTTON;
     }
 
-    static const char* getDbusObjectPath()
+    static constexpr std::string getDbusObjectPath()
     {
         return DBG_HS_DBUS_OBJECT_NAME;
     }

inc/hostSelector_switch.hpp
@@ -15,7 +15,7 @@
 #include <fstream>
 #include <iostream>
 
-static constexpr std::string_view HOST_SELECTOR = "HOST_SELECTOR";
+static constexpr auto HOST_SELECTOR = "HOST_SELECTOR";
 
 static constexpr auto INVALID_INDEX = std::numeric_limits<size_t>::max();
 
@@ -51,12 +51,12 @@ class HostSelector final :
         deInit();
     }
 
-    static constexpr std::string_view getFormFactorName()
+    static constexpr std::string getFormFactorName()
     {
         return HOST_SELECTOR;
     }
 
-    static const char* getDbusObjectPath()
+    static constexpr std::string getDbusObjectPath()
     {
         return HS_DBUS_OBJECT_NAME;
     }

inc/host_then_chassis_poweroff.hpp
@@ -61,7 +61,7 @@ class HostThenChassisPowerOff : public PowerButtonProfile
      * @brief Returns the name that matches the value in
      *        meson_options.txt.
      */
-    static constexpr std::string_view getName()
+    static constexpr std::string getName()
     {
         return "host_then_chassis_poweroff";
     }

inc/id_button.hpp
@@ -27,7 +27,7 @@
 
 #include <phosphor-logging/elog-errors.hpp>
 
-static constexpr std::string_view ID_BUTTON = "ID_BTN";
+static constexpr auto ID_BUTTON = "ID_BTN";
 
 class IDButton :
     public sdbusplus::server::object_t<
@@ -52,11 +52,11 @@ class IDButton :
 
     void simPress() override;
 
-    static constexpr std::string_view getFormFactorName()
+    static constexpr std::string getFormFactorName()
     {
         return ID_BUTTON;
     }
-    static constexpr const char* getDbusObjectPath()
+    static constexpr std::string getDbusObjectPath()
     {
         return ID_DBUS_OBJECT_NAME;
     }

inc/power_button.hpp
@@ -29,7 +29,7 @@
 
 #include <chrono>
 
-static constexpr std::string_view POWER_BUTTON = "POWER_BUTTON";
+static constexpr auto POWER_BUTTON = "POWER_BUTTON";
 
 class PowerButton :
     public sdbusplus::server::object_t<
@@ -55,11 +55,11 @@ class PowerButton :
     void simPress() override;
     void simLongPress() override;
 
-    static constexpr std::string_view getFormFactorName()
+    static constexpr std::string getFormFactorName()
     {
         return POWER_BUTTON;
     }
-    static constexpr const char* getDbusObjectPath()
+    static constexpr std::string getDbusObjectPath()
     {
         return POWER_DBUS_OBJECT_NAME;
     }

inc/power_button_profile_factory.hpp
@@ -33,7 +33,7 @@ class PowerButtonProfileFactory
     template <typename T>
     void addToRegistry()
     {
-        profileRegistry[std::string(T::getName())] = [](sdbusplus::bus_t& bus) {
+        profileRegistry[T::getName()] = [](sdbusplus::bus_t& bus) {
             return std::make_unique<T>(bus);
         };
     }

commit ea47dd49799e74f082a0a1386efac9bdfd6aaba4

Don't need indexed button name if only 1 chassis
The commit '31ce375 Support multi power buttons with multi behaviors'
created an assumption that the power button name in gpio_defs.json would
always have a chassis instance number appended to it, and would add an
indexed name as a button factory map key.

That means when ButtonFactor::createInstance() is called with a power
button name without an index, it would not create a PowerButton instance
and button presses wouldn't work.

Instead of changing all of the existing single chassis gpio_defs.json
files in openbmc/openbmc, instead change the factory registration
wrapper code to only register that button as a indexed one if the
provided meson option contains more than 1 chassis.

Tested:
A power button press works to power on the system again.

Fixes: 31ce375e7e71 ("Support multi power buttons with multi behaviors")
Change-Id: I15d02a811aae592adf36c3ac07afd0fc6c15450a
Signed-off-by: Matt Spinler <spinler@us.ibm.com>

inc/button_factory.hpp
@@ -2,6 +2,7 @@
 
 #include "button_config.hpp"
 #include "button_interface.hpp"
+#include "config.hpp"
 
 #include <phosphor-logging/elog-errors.hpp>
 
@@ -93,12 +94,21 @@ class ButtonIFRegister
 
     explicit ButtonIFRegister(size_t count)
     {
-        // register the class factory function
-        // The index, 'countIter', starts at 1 and increments,
-        // representing slot_1 through slot_N.
-        for (size_t countIter = 1; countIter <= count; countIter++)
+        // The JSON power button definitions only have an instance in
+        // their name if there is more than 1 chassis.
+        if (instances.size() > 1)
         {
-            ButtonFactory::instance().addToRegistry<T>(countIter);
+            // register the class factory function
+            // The index, 'countIter', starts at 1 and increments,
+            // representing slot_1 through slot_N.
+            for (size_t countIter = 1; countIter <= count; countIter++)
+            {
+                ButtonFactory::instance().addToRegistry<T>(countIter);
+            }
+        }
+        else
+        {
+            ButtonFactory::instance().addToRegistry<T>();
         }
     }
 };